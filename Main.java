c414ebf23b4040adb19df09100c8b5dcc1b5b8fb
paulpladziewicz <ppladziewicz@gmail.com>
2025-01-15 07:37:22 -0500

    updating code style

--- Main.java
+++ Main.java
@@ -1,24 +1,24 @@
 import java.lang.reflect.Method;
 
 public class Main {
+  @SimpleAnnotation(name = "Paul", age = 3)
+  public static void main(String[] args) {
+    Class<?> clazz = Main.class; // could also use getClass() with an instantiated class object
 
-    @SimpleAnnotation(name = "Paul", age = 3)
-    public static void main(String[] args) {
-        Class<?> clazz = Main.class; // could also use getClass() with an instantiated class object
+    for (Method method : clazz.getMethods()) {
+      SimpleAnnotation simpleAnnotation = method.getAnnotation(SimpleAnnotation.class);
 
-        for (Method method : clazz.getMethods()) {
-            SimpleAnnotation simpleAnnotation = method.getAnnotation(SimpleAnnotation.class);
-
-            if (simpleAnnotation != null) {
-                if (!simpleAnnotation.name().isEmpty()) {
-                    System.out.println(simpleAnnotation.name());
-                } else if (!simpleAnnotation.value().isEmpty()) {
-                    System.out.println(simpleAnnotation.value());
-                }
-                if (simpleAnnotation.age() > 0) {
-                    System.out.println(simpleAnnotation.age());
-                }
-            }
+      if (simpleAnnotation != null) {
+        if (!simpleAnnotation.name().isEmpty()) {
+          System.out.println(simpleAnnotation.name());
+        } else if (!simpleAnnotation.value().isEmpty()) {
+          System.out.println(simpleAnnotation.value());
         }
+
+        if (simpleAnnotation.age() > 0) {
+          System.out.println(simpleAnnotation.age());
+        }
+      }
     }
+  }
 }
--- SimpleAnnotation.java
+++ SimpleAnnotation.java
@@ -3,7 +3,7 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SimpleAnnotation {
-    String value() default ""; // person's name, default attribute if one value passed
-    String name() default ""; // defaults help avoid null checks
-    int age() default -1;
+  String value() default ""; // person's name, default attribute if one value passed
+  String name() default ""; // defaults help avoid null checks
+  int age() default -1;
 }
